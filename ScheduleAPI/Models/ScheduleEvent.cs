7ff7c6c2814a683417ff102ab146d56bf0475cf6
0leXis <alexbashur@gmail.com>
2021-08-02 16:14:18 +0300

    ScheduleEvent model

---
+++ ScheduleAPI/Models/ScheduleEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScheduleAPI.Models
+{
+    public class ScheduleEvent
+    {
+        public int Id { get; set; }
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string EventDescription { get; set; }
+    }
+}
