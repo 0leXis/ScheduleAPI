619a639b1ca2e021165f05d681d5687a6227e166
0leXis <alexbashur@gmail.com>
2021-08-02 18:15:53 +0300

    EventsController

---
+++ ScheduleAPI/Controllers/EventsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ScheduleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private ScheduleContext _db;
+        public EventsController(ScheduleContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ScheduleEvent>>> Get()
+        {
+            return await _db.ScheduleEvents.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ScheduleEvent>> Get(int id)
+        {
+            return await _db.ScheduleEvents.SingleAsync(x => x.Id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<ScheduleEvent>>> Post(ScheduleEvent scheduleEvent)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            _db.ScheduleEvents.Add(scheduleEvent);
+            await _db.SaveChangesAsync();
+            return Ok(scheduleEvent);
+        }
+        [HttpPut]
+        public async Task<ActionResult<IEnumerable<ScheduleEvent>>> Put(ScheduleEvent scheduleEvent)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_db.ScheduleEvents.Any(x => x.Id == scheduleEvent.Id))
+                return NotFound();
+            _db.Update(scheduleEvent);
+            await _db.SaveChangesAsync();
+            return Ok(scheduleEvent);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<IEnumerable<ScheduleEvent>>> Delete(int id)
+        {
+            var scheduleEvent = _db.ScheduleEvents.FirstOrDefault(x => x.Id == id);
+            if (scheduleEvent == null)
+                return NotFound();
+            _db.ScheduleEvents.Remove(scheduleEvent);
+            await _db.SaveChangesAsync();
+            return Ok(scheduleEvent);
+        }
+    }
+}
--- ScheduleAPI/Startup.cs
+++ ScheduleAPI/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ScheduleAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +27,8 @@ namespace ScheduleAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ScheduleContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
