2b4bf96b07fc016b6a3933aa82f0a9df0a2d136d
0leXis <alexbashur@gmail.com>
2021-08-02 16:18:04 +0300

    Db context created

---
+++ ScheduleAPI/Models/ScheduleContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScheduleAPI.Models
+{
+    public class ScheduleContext : DbContext
+    {
+        public DbSet<ScheduleEvent> ScheduleEvents { get; set; }
+        public ScheduleContext(DbContextOptions<ScheduleContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
